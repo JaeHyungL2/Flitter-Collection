8d3ec9489f41bb1449519342bb6a2a599991a440
JaeHyungL2 <wednestues18@gmail.com>
2024-03-19 11:47:36 +0900

    chap05_flutter_api test

---
+++ chap05_flutter_api/android/app/src/main/kotlin/com/example/chap05_flutter_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chap05_flutter_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chap05_flutter_api/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
+
+//매일하던거 하면되고. 커밋하자.
